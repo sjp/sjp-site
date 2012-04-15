1bed8a1e921461b6e3c16fe6b37fc734b7b8b1b4
Simon Potter <simon@sjp.co.nz>
2012-04-15 22:43:10 +1200

    Another update, mostly gridsvg additions, among other things.

--- content/scripts/gridsvg-slider.js
+++ content/scripts/gridsvg-slider.js
@@ -1,15 +1,17 @@
-$('#toggle').click(function() {
-    // Toggling between Show/Hide text on link
-    var str = $('#toggle').text();
-    if (str.charAt(0) === 'S') {
-        $('#toggle').html('Hide' + str.substring(7));
-    } else {
-        $('#toggle').html('Show me' + str.substring(4));
-    }
+(function() {
+    $('#toggle').click(function() {
+        // Toggling between Show/Hide text on link
+        var str = $('#toggle').text();
+        if (str.charAt(0) === 'S') {
+            $('#toggle').html('Hide' + str.substring(7));
+        } else {
+            $('#toggle').html('Show me' + str.substring(4));
+        }
+        
+        // Sliding the code up/down
+        $('#gridsvg-code').slideToggle('fast', function() {});
     
-    // Sliding the code up/down
-    $('#gridsvg-code').slideToggle('fast', function() {});
-
-    // Stopping the browser from loading the URL
-    return false;
-});
+        // Stopping the browser from loading the URL
+        return false;
+    });
+})();
