033d7ad22045ae9dedaac3a34dbf5f5d7eafa798
Simon Potter <simon@sjp.co.nz>
2013-01-10 02:04:19 +1300

    Adding TimingManager TR to site.

---
+++ projects/timing-manager/simple-anim-timing.js
@@ -0,0 +1,38 @@
+var simpleAnim = [
+ {
+ "start":      0,
+"durn":      1,
+"label": "red",
+"vec": "complete",
+"vecNum": 1,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":      2,
+"durn":      1,
+"label": "green",
+"vec": "complete",
+"vecNum": 2,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":      4,
+"durn":      1,
+"label": "blue",
+"vec": "complete",
+"vecNum": 3,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":      6,
+"durn":      3,
+"label": "final",
+"vec": "complete",
+"vecNum": 4,
+"trac": null,
+"tracNum": null 
+} 
+]; 
---
+++ projects/timing-manager/timing-data.js
@@ -0,0 +1,137 @@
+var timingData = [
+ {
+ "start":      0,
+"durn":      1,
+"label": "Alpha",
+"vec": "Mike",
+"vecNum": 1,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":      1,
+"durn":      2,
+"label": "Delta",
+"vec": "Mike",
+"vecNum": 2,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":      3,
+"durn":      2,
+"label": "Bravo",
+"vec": "Mike",
+"vecNum": 3,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":      5,
+"durn":      2,
+"label": "Delta",
+"vec": "Mike",
+"vecNum": 4,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":      7,
+"durn":      3,
+"label": "Charlie",
+"vec": "Mike",
+"vecNum": 5,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":     10,
+"durn":      1,
+"label": "Alpha",
+"vec": "Mike",
+"vecNum": 6,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":     11,
+"durn":      2,
+"label": "Delta",
+"vec": "Mike",
+"vecNum": 7,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":     13,
+"durn":      2,
+"label": "Bravo",
+"vec": "Mike",
+"vecNum": 8,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":     15,
+"durn":      2,
+"label": "Delta",
+"vec": "Mike",
+"vecNum": 9,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":     17,
+"durn":      3,
+"label": "Charlie",
+"vec": "Mike",
+"vecNum": 10,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":     20,
+"durn":      1,
+"label": "Alpha",
+"vec": "Mike",
+"vecNum": 11,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":     21,
+"durn":      2,
+"label": "Delta",
+"vec": "Mike",
+"vecNum": 12,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":     23,
+"durn":      2,
+"label": "Bravo",
+"vec": "Mike",
+"vecNum": 13,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":     25,
+"durn":      2,
+"label": "Delta",
+"vec": "Mike",
+"vecNum": 14,
+"trac": null,
+"tracNum": null 
+},
+{
+ "start":     27,
+"durn":      3,
+"label": "Charlie",
+"vec": "Mike",
+"vecNum": 15,
+"trac": null,
+"tracNum": null 
+} 
+]; 
