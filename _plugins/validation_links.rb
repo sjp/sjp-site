8d180f4854908512ab9f1748ce46d28a27a22b3f
Simon Potter <simon@sjp.co.nz>
2014-02-07 01:09:12 +1300

    Encode & correctly.

--- _plugins/validation_links.rb
+++ _plugins/validation_links.rb
@@ -6,7 +6,7 @@ module Jekyll
       host = CGI::escape(context.environments.first["site"]["url"])
       resource = CGI::escape(context.environments.first["page"]["url"])
       url = "#{host}#{resource}"
-      %|<a href="http://validator.w3.org/check?uri=#{url}">Valid HTML5</a> &amp; <a href="http://jigsaw.w3.org/css-validator/validator?uri=#{url}&profile=css3">Valid CSS3</a>|
+      %|<a href="http://validator.w3.org/check?uri=#{url}">Valid HTML5</a> &amp; <a href="http://jigsaw.w3.org/css-validator/validator?uri=#{url}&amp;profile=css3">Valid CSS3</a>|
     end
   end
 end
