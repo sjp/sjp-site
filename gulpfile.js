9eb4a8aac39472d27e631a302aa7a2ef73e5e229
Simon Potter <simon@sjp.co.nz>
2016-01-27 22:27:51 +1300

    Simplify main gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,5 @@
 var gulp = require('gulp');
-var requireDir = require('require-dir');
-var rename = require('gulp-rename');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var shell = require('gulp-shell');
-var sass = require('gulp-sass');
-
-var tasksDir = requireDir('./_tasks');
+var tasksDir = require('require-dir')('_tasks');
 
 gulp.task('default', ['styles', 'scripts', 'timing-manager', 'rainbow'], function() {
 
