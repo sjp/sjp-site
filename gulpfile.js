90c025391e3a59043330e9ce22badc1557646a3d
Simon Potter <simon@sjp.co.nz>
2016-01-28 22:42:32 +1300

    Drop jekyll integration in gulp build.
    Enables CI builds to report errors in the jekyll step.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
 var gulp = require('gulp');
-var spawn = require('child_process').spawn;
 var tasksDir = require('require-dir')('_tasks');
 
-gulp.task('jekyll', function() {
-    return spawn('jekyll', ['build'], { stdio: 'inherit' });
-});
-
-gulp.task('build:dev', ['styles:dev', 'scripts:dev', 'rainbow:dev', 'timing-manager', 'jekyll'], function() {
+gulp.task('build:dev', ['styles:dev', 'scripts:dev', 'rainbow:dev', 'timing-manager'], function() {
 
 });
 
-gulp.task('build:prod', ['styles:prod', 'scripts:prod', 'rainbow:prod', 'timing-manager', 'jekyll'], function() {
+gulp.task('build:prod', ['styles:prod', 'scripts:prod', 'rainbow:prod', 'timing-manager'], function() {
 
 });
 
