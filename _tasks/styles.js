43afedac3044d7f5683adf9e3c3b910830c20ebc
Simon Potter <simon@sjp.co.nz>
2016-02-08 21:03:00 +1300

    Update to support newest versions of bourbon/neat/bitters.
    Also updating normalize.css and integrating postcss into build.

--- _tasks/styles.js
+++ _tasks/styles.js
@@ -1,16 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('styles:dev', function() {
     return gulp.src('_styles/*.scss')
         .pipe(sass())
+        .pipe(postcss([
+            cssnext,
+            autoprefixer
+        ]))
         .pipe(gulp.dest('_site/styles'));
 });
 
 gulp.task('styles:prod', function() {
     return gulp.src('_styles/*.scss')
         .pipe(sass())
+        .pipe(postcss([
+            cssnext,
+            autoprefixer
+        ]))
         .pipe(cssnano())
         .pipe(gulp.dest('_site/styles'));
 });
